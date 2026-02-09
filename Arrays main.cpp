e462f67ee22823d842e887dbbb4625c9d035c09e
elghshada-lgtm <elghshada@gmail.com>
2026-02-08 22:37:06 -0800

    Add main.cpp to demonstrate array usage in C++

---
+++ Arrays main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    //Task 1
+    int numbers[100];
+
+    cout << "Array of 100 integers created." << endl;
+
+    //Task 2
+    cout << "Size of one element: "
+         << sizeof(numbers[0]) << " bytes" << endl;
+
+    //Task 5
+    cout << "Array base address: " << numbers << endl;
+    cout << "First element address: " << &numbers[0] << endl;
+
+    return 0;
+}
